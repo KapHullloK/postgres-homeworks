73971e7b9f7908526b626ee07dbfcb1dadf5c592
afro_0 <kirillov2136@gmail.com>
2024-06-10 11:13:20 +0300

    do hw_4

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,32 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
 
+alter table products
+    add constraint new_price check (products.unit_price > 0);
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
+
+alter table products
+    add constraint new_discontinued check (products.discontinued in (0, 1));
 
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
+create table withdrawn as
+select *
+from products
+where products.discontinued = 1;
+
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+
+
+delete
+from order_details
+where order_details.product_id in (select products.product_id from products where discontinued = 1);
+
+delete
+from products
+where products.discontinued = 1;
+
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,43 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+create table student
+(
+    student_id serial,
+    first_name varchar,
+    last_name  varchar,
+    birthday   date,
+    phone      varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
+
+alter table student
+    add column middle_name varchar;
 
 
 -- 3. Удалить колонку middle_name
 
+alter table student
+    drop column middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
 
+alter table student
+    rename column birthday to birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
+
+alter table student
+    alter column phone set data type varchar(32);
 
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
 
+insert into student (first_name, last_name, phone)
+values ('Bob1', 'Shazy1', '18652331814'),
+       ('Bob2', 'Shazy2', '15652331814'),
+       ('Bob3', 'Shazy3', '16652331814');
+
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+truncate table student restart identity;
